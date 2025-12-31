5fae6d0ae7c1e00ba14bcf9e49144fde032162e3
orzech <orzech363@gmail.com>
2025-12-31 17:38:52 +0100

    Fix Prisma configuration for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['szoniska.pl', 'lh3.googleusercontent.com', 'cdn.discordapp.com'],
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
 };
 
 export default nextConfig;
